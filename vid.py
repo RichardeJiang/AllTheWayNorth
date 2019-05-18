3d3c1ac57ef4012a77e11712ea8482360e3372cf
richardejiang <richardejiang@hotmail.com>
2019-05-18 15:08:49 -0400

    output image restored to original ratio; color adjusted

--- vid.py
+++ vid.py
@@ -17,11 +17,11 @@ def save_image(data, fname="img1.png", swap_channel=True):
     cv2.imwrite(fname, data)
 
 def draw_label(image, point, label, font=cv2.FONT_HERSHEY_SIMPLEX,
-               font_scale=0.5, thickness=2):
+               font_scale=0.4, thickness=1):
     size = cv2.getTextSize(label, font, font_scale, thickness)[0]
     x, y = point
     cv2.rectangle(image, (x, y - size[1]),
-                  (x + size[0], y), (255, 0, 0), cv2.FILLED)
+                  (x + size[0], y), (34, 34, 178), cv2.FILLED)
     cv2.putText(image, label, point, font, font_scale,
                 (255, 255, 255), thickness)
 
@@ -99,39 +99,17 @@ def non_max_suppression(boxes, probs=None, nms_threshold=0.3):
     # return only the bounding boxes indexes
     return pick
 
-def gstreamer_pipeline (capture_width=640, capture_height=360, display_width=640, display_height=360, framerate=5, flip_method=0) :   
-    return ('nvarguscamerasrc ! ' 
-    'video/x-raw(memory:NVMM), '
-    'width=(int)%d, height=(int)%d, '
-    'format=(string)NV12, framerate=(fraction)%d/1 ! '
-    'nvvidconv flip-method=%d ! '
-    'video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! '
-    'videoconvert ! '
-    'video/x-raw, format=(string)BGR ! appsink'  % (capture_width,capture_height,framerate,flip_method,display_width,display_height))
-
-def show_camera():
-    # To flip the image, modify the flip_method parameter (0 and 2 are the most common)
-    print(gstreamer_pipeline(flip_method=0))
-    cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
-    if cap.isOpened():
-        window_handle = cv2.namedWindow('CSI Camera', cv2.WINDOW_AUTOSIZE)
-        # Window 
-        while cv2.getWindowProperty('CSI Camera',0) >= 0:
-            ret_val, img = cap.read()
-            cv2.imshow('CSI Camera',img)
-	    # This also acts as 
-            keyCode = cv2.waitKey(30) & 0xff
-            # Stop the program on the ESC key
-            if keyCode == 27:
-               break
-        cap.release()
-        cv2.destroyAllWindows()
-    else:
-        print('Unable to open camera')
-
 if __name__ == "__main__":
     
     input_names = ['image_tensor']
+
+    # what we want from COCO labels: vehicles
+    desired_classes_names = ["truck", "bus", "motorcycle", "car", "bicycle"]
+    desired_classes = {3:"car", 2:"bicycle", 4:"motorcycle", 6:"bus", 8:"truck"}
+
+    # calculation: SSD input dimension: 300 x 300; we want output window: (1280 / 720)*300 x 300
+    standard_width = int((1280 / 720.0) * 300.0)
+    standard_height = 300
 
     # The TensorRT inference graph file downloaded from Colab or your local machine.
     pb_fname = "trt_graph1.pb"
@@ -173,7 +151,9 @@ if __name__ == "__main__":
         Inside the main processing loop, do stuff
         """
 
+        image_to_show = cv2.resize(image, (standard_width, standard_height))
         image = cv2.resize(image, (300, 300))
+        
 
         """
         Original inference step
@@ -184,14 +164,22 @@ if __name__ == "__main__":
         boxes = boxes[0]  # index by 0 to remove batch dimension
         scores = scores[0]
         classes = classes[0]
-        num_detections = int(num_detections[0])
+        # num_detections = int(num_detections[0])
+
+
+        # retain only those detection we are interested in: vehicles
+        desired_detection_idx = np.isin(classes, list(desired_classes.keys()))
+        boxes = boxes[desired_detection_idx]
+        scores = scores[desired_detection_idx]
+        classes = classes[desired_detection_idx]
+        num_detections = len(scores)
 
         # Boxes unit in pixels (image coordinates).
         boxes_pixels = []
         for i in range(num_detections):
             # scale box to image coordinates
-            box = boxes[i] * np.array([image.shape[0],
-                                    image.shape[1], image.shape[0], image.shape[1]])
+            box = boxes[i] * np.array([image_to_show.shape[0],
+                                    image_to_show.shape[1], image_to_show.shape[0], image_to_show.shape[1]])
             box = np.round(box).astype(int)
             boxes_pixels.append(box)
         boxes_pixels = np.array(boxes_pixels)
@@ -205,17 +193,18 @@ if __name__ == "__main__":
             box = boxes_pixels[i]
             box = np.round(box).astype(int)
             # Draw bounding box.
-            image = cv2.rectangle(
-                image, (box[1], box[0]), (box[3], box[2]), (0, 255, 0), 2)
-            label = "{}:{:.2f}".format(int(classes[i]), scores[i])
+            image_to_show = cv2.rectangle(
+                image_to_show, (box[1], box[0]), (box[3], box[2]), (0, 255, 0), 2)
+            # label = "{}:{:.2f}".format(int(classes[i]), scores[i])
+            label = desired_classes[classes[i]] + " {:.2f}".format(scores[i])
             # Draw label (class index and probability).
-            draw_label(image, (box[1], box[0]), label)
+            draw_label(image_to_show, (box[1], box[0] - 2), label)
 
         """
         End of original inference step
         """
 
-        cv2.imshow('CSI Camera',image)
+        cv2.imshow('video input result',image_to_show)
         # This also acts as 
         keyCode = cv2.waitKey(30) & 0xff
         # Stop the program on the ESC key
@@ -234,70 +223,5 @@ if __name__ == "__main__":
     tf_sess.close()
     cap.release()
     cv2.destroyAllWindows()
-
-    # if cap.isOpened():
-    #     window_handle = cv2.namedWindow('CSI Camera', cv2.WINDOW_AUTOSIZE)
-    #     # Window 
-    #     while cv2.getWindowProperty('CSI Camera',0) >= 0:
-    #         ret_val, image = cap.read()
-    #         now = time.time()
-    #         image = cv2.resize(image, (300, 300))
-
-    #         """
-    #         Original inference step
-    #         """
-    #         scores, boxes, classes, num_detections = tf_sess.run([tf_scores, tf_boxes, tf_classes, tf_num_detections], feed_dict={
-    #             tf_input: image[None, ...]
-    #         })
-    #         boxes = boxes[0]  # index by 0 to remove batch dimension
-    #         scores = scores[0]
-    #         classes = classes[0]
-    #         num_detections = int(num_detections[0])
-
-    #         # Boxes unit in pixels (image coordinates).
-    #         boxes_pixels = []
-    #         for i in range(num_detections):
-    #             # scale box to image coordinates
-    #             box = boxes[i] * np.array([image.shape[0],
-    #                                     image.shape[1], image.shape[0], image.shape[1]])
-    #             box = np.round(box).astype(int)
-    #             boxes_pixels.append(box)
-    #         boxes_pixels = np.array(boxes_pixels)
-
-    #         # Remove overlapping boxes with non-max suppression, return picked indexes.
-    #         pick = non_max_suppression(boxes_pixels, scores[:num_detections], 0.5)
-    #         # print(pick)
-
-
-    #         for i in pick:
-    #             box = boxes_pixels[i]
-    #             box = np.round(box).astype(int)
-    #             # Draw bounding box.
-    #             image = cv2.rectangle(
-    #                 image, (box[1], box[0]), (box[3], box[2]), (0, 255, 0), 2)
-    #             label = "{}:{:.2f}".format(int(classes[i]), scores[i])
-    #             # Draw label (class index and probability).
-    #             draw_label(image, (box[1], box[0]), label)
-
-    #         """
-    #         End of original inference step
-    #         """
-
-    #         cv2.imshow('CSI Camera',image)
-	#     # This also acts as 
-    #         keyCode = cv2.waitKey(30) & 0xff
-    #         # Stop the program on the ESC key
-    #         if keyCode == 27:
-    #            break
-    #     tf_sess.close()
-    #     cap.release()
-    #     cv2.destroyAllWindows()
-    # else:
-    #     print('Unable to open camera')
-
-
-    """
-    End of camera module
-    """
 
     pass
